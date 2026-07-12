6cc46072467b69401f23ba1f62c23a3dfe49d323
ehancup <mondokskuy19@gmail.com>
2026-07-12 16:48:44 +0700

    setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { RouterProvider } from 'react-router'
-import { router } from './router.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { RouterProvider } from "react-router";
+import { router } from "./router.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}/>
-)
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
