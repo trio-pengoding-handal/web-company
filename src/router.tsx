c52752bc864d183f455c12eb40fc4b178f5a10a1
ehancup <mondokskuy19@gmail.com>
2026-07-12 16:44:12 +0700

    setup tailwind

--- src/router.tsx
+++ src/router.tsx
@@ -4,4 +4,7 @@ import App from "./App";
 export const router = createBrowserRouter([{
     path: '/',
     Component: App
+},{
+    path: '/test-tailwind',
+    Component: () => <p className="text-yellow-400 font-bold">test</p>
 }])
