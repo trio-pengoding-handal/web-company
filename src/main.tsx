7315723407c90fdb7d263e8aa59d02bb9f85f43f
ehancup <mondokskuy19@gmail.com>
2026-07-12 16:25:54 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './router.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router}/>
+)
---
+++ src/router.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+export const router = createBrowserRouter([{
+    path: '/',
+    Component: App
+}])
