1668826b30dcaa67aba78c9a8985eab22d059593
ehancup <mondokskuy19@gmail.com>
2026-07-12 16:44:30 +0700

    format

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
 
-export const router = createBrowserRouter([{
-    path: '/',
-    Component: App
-},{
-    path: '/test-tailwind',
-    Component: () => <p className="text-yellow-400 font-bold">test</p>
-}])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/test-tailwind",
+    Component: () => <p className="text-yellow-400 font-bold">test</p>,
+  },
+]);
